d09b83a5124b2bab7d86b8cb96c60771672e9838
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2025-03-12 11:11:43 +0100

    to vitest

--- tiles-editor/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
-  transform: {
-    "^.+\\.[tj]sx?$": "babel-jest",
-  },
-};
