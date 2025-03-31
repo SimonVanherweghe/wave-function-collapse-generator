572a345a9ff7ec95a960aa6bd86d550c23bb7b88
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2025-03-31 22:23:38 +0200

    base

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/wave-function-collapse-generator",
   test: {
     globals: true,
     environment: "jsdom",
     css: false,
-    setupFiles: './src/setupTests.js',
-  }
-})
+    setupFiles: "./src/setupTests.js",
+  },
+});
